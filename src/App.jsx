1e1ad7477b30f8214792a50ccf68bcc9fe72ca2e
The Mvp Lab <productmvplab@gmail.com>
2026-03-12 21:25:00 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import NavigationTracker from '@/lib/NavigationTracker'
 import { pagesConfig } from './pages.config'
+import PropertyOwnerSignup from './pages/PropertyOwnerSignup'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
