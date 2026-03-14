09767b5bcd778b4e6a2d7a48d991ee3eb283d514
The Mvp Lab <productmvplab@gmail.com>
2026-03-14 01:52:47 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -5,7 +5,6 @@ import { Building2 } from 'lucide-react';
 export default function Layout({ children, currentPageName }) {
   return (
     <div className="min-h-screen flex flex-col">
-      <Header />
       <main className="flex-1">
         {children}
       </main>
