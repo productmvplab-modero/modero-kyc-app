e525b45c5d8a5fabe308ed431752341da7bac7cd
Modero KYC <productmvplab@gmail.com>
2026-03-21 07:22:41 +0000

    File changes

--- src/pages/TenantQualification.jsx
+++ src/pages/TenantQualification.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import Step1Profile from '@/components/qualification/Step1Profile';
 import Step2Personal from '@/components/qualification/Step2Personal';
 import Step3Employment from '@/components/qualification/Step3Employment';
