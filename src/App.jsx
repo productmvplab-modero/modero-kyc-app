8b45762ce56679d4c540379ef9242d82b4e81d11
Modero KYC <productmvplab@gmail.com>
2026-03-21 10:03:41 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import ViewingBooking from './pages/ViewingBooking'
 import ApartmentViewing from './pages/ApartmentViewing'
 import ContractManager from './pages/ContractManager'
 import ContractSignature from './pages/ContractSignature'
+import MessagingCenter from './pages/MessagingCenter'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
